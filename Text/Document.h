1703abfa16e0418227a896bec4c8c944c93507ef
Dima Pilipenko <dimap09@gmail.com>
2013-03-07 17:03:24 -0500

    Initial Commit

---
+++ Text/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  Text
+//
+//  Created by Dmitriy Pilipenko on 3/7/13.
+//  Copyright (c) 2013 Dmitriy Pilipenko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ Text/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Text
+//
+//  Created by Dmitriy Pilipenko on 3/7/13.
+//  Copyright (c) 2013 Dmitriy Pilipenko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
